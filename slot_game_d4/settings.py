a0e1c9e001213df6d25ef95e9410d0c62235f738
Ryan <oocpuxo@gmail.com>
2022-11-17 16:37:38 -0500

    Adding WhiteNoise Adding WhiteNoise to handle static files.

--- slot_game_d4/settings.py
+++ slot_game_d4/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['rcpux-slot-game.herokuapp.com']
 
 INSTALLED_APPS = [
     'site_app',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
